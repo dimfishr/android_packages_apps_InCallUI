fb6cf83c469bad03c2e5f537ca598eb6f980d157
Sasikumar D R <sasikumardr@gmail.com>
2014-01-07 21:46:10 +0530

    [3/3] InCallUI: Flip to Mute/Reject Call
    Actual Logic for this functionality.
    
    PS2: Remove unwanted space
    
    Base: https://gerrit.omnirom.org/#/c/4668/
    Telephony: https://gerrit.omnirom.org/#/c/4670/
    
    Change-Id: Ia10df0045e9919c6b7efc97857abaeba246c365d
    
    InCallUI: Flip to Mute/Reject Call cleanup: fix ITelephony access
    
    Using reflection to access ITelephony is a strange idea
    
    Change-Id: Ia34b135460cc2b85fbd5db4585228a015c2a52b1

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -61,6 +61,7 @@ public class InCallPresenter implements CallList.Listener {
     private CallList mCallList;
     private InCallActivity mInCallActivity;
     private InCallState mInCallState = InCallState.NO_CALLS;
+    private AccelerometerListener mAccelerometerListener;
     private ProximitySensor mProximitySensor;
     private boolean mServiceConnected = false;
     private static String LOG_TAG = "InCallPresenter";
@@ -139,6 +140,8 @@ public class InCallPresenter implements CallList.Listener {
 
         mProximitySensor = new ProximitySensor(context, mAudioModeProvider);
         addListener(mProximitySensor);
+
+        mAccelerometerListener = new AccelerometerListener(context);
 
         mCallList = callList;
 
@@ -344,6 +347,9 @@ public class InCallPresenter implements CallList.Listener {
         // Renable notification shade and soft navigation buttons, if we are no longer in the
         // incoming call screen
         if (!newState.isIncoming()) {
+            if(mAccelerometerListener != null){
+                mAccelerometerListener.enableSensor(false);
+            }
             CallCommandClient.getInstance().setSystemBarNavigationEnabled(true);
         }
 
@@ -394,6 +400,9 @@ public class InCallPresenter implements CallList.Listener {
         // Disable notification shade and soft navigation buttons
         if (newState.isIncoming()) {
             CallCommandClient.getInstance().setSystemBarNavigationEnabled(false);
+            if(mAccelerometerListener != null){
+                mAccelerometerListener.enableSensor(true);
+            }
         }
 
         for (IncomingCallListener listener : mIncomingCallListeners) {
@@ -594,6 +603,8 @@ public class InCallPresenter implements CallList.Listener {
         // (1) Attempt to answer a call
         if (incomingCall != null) {
             CallCommandClient.getInstance().answerCall(incomingCall.getCallId());
+            if(mAccelerometerListener != null)
+                mAccelerometerListener.enableSensor(false);
             return true;
         }
 
@@ -840,6 +851,8 @@ public class InCallPresenter implements CallList.Listener {
                 mProximitySensor.tearDown();
             }
             mProximitySensor = null;
+
+            mAccelerometerListener = null;
 
             mAudioModeProvider = null;
 
