d4a8c88872afb04c289a73a78a96f621da9571e9
dimfishr <dimfish@gmail.com>
2014-06-16 16:43:01 +0400

    Revert "Merge "incallui: make navigation bar translucent + other fixes" into cm-11.0"
    This reverts commit acce9b9390d7db3f15d58060cc3211b496bb95b2, reversing
    changes made to db221939fde437d56870dd89281d1fad83e8ecdb.
    
    Conflicts:
    	res/layout/primary_call_info.xml

--- src/com/android/incallui/InCallActivity.java
+++ src/com/android/incallui/InCallActivity.java
@@ -74,7 +74,6 @@ public class InCallActivity extends Activity {
 
     /** Use to pass 'showDialpad' from {@link #onNewIntent} to {@link #onResume} */
     private boolean mShowDialpadRequested;
-    private boolean mConferenceManagerShown;
 
     // This enum maps to Phone.SuppService defined in telephony
     private enum SuppService {
@@ -169,7 +168,6 @@ public class InCallActivity extends Activity {
             mCallButtonFragment.displayDialpad(true);
             mShowDialpadRequested = false;
         }
-        updateSystemBarTranslucency();
         if (mCoverWindowCoords != null) {
             registerReceiver(mLidStateChangeReceiver, new IntentFilter(
                     WindowManagerPolicy.ACTION_LID_STATE_CHANGED));
@@ -299,8 +297,6 @@ public class InCallActivity extends Activity {
             return;
         } else if (mConferenceManagerFragment.isVisible()) {
             mConferenceManagerFragment.setVisible(false);
-            mConferenceManagerShown = false;
-            updateSystemBarTranslucency();
             return;
         }
 
@@ -578,30 +574,7 @@ public class InCallActivity extends Activity {
     public void displayManageConferencePanel(boolean showPanel) {
         if (showPanel) {
             mConferenceManagerFragment.setVisible(true);
-            mConferenceManagerShown = true;
-            updateSystemBarTranslucency();
-        } else {
-            mConferenceManagerFragment.setVisible(false);
         }
-    }
-
-    public void onManageConferenceDoneClicked() {
-        if (mConferenceManagerShown && !mConferenceManagerFragment.isVisible()) {
-            mConferenceManagerShown = false;
-            updateSystemBarTranslucency();
-        }
-    }
-
-    private void updateSystemBarTranslucency() {
-        final boolean doTranslucency = !mConferenceManagerShown;
-        final Window window = getWindow();
-
-        if (doTranslucency) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        } else {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
-        window.getDecorView().requestFitSystemWindows();
     }
 
     // The function is called when Modify Call button gets pressed.
